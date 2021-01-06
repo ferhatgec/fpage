3de9857e52c51b9761d32ad0d0394deecf0550da
ferhatgec <ferhatgectao@gmail.com>
2021-01-06 16:51:19 +0300

    Add: install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+c++ -std=c++17 -I./libraries/ -I./include/ src/Parser.cpp src/FPage.cpp -o /bin/fpage
+
+# Create directory
+sudo mkdir /usr/local/fpages/
+
+# Copy all pages to /usr/local/pages/
+sudo rsync -a fpages/. /usr/local/fpages/
